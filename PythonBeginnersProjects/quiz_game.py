32a6faf659a1bc39e0b861c079c099a52d2c4848
redfoxbg <redfoxbg@gmail.com>
2023-04-24 16:53:56 +0300

    test

---
+++ PythonBeginnersProjects/quiz_game.py
@@ -0,0 +1,43 @@
+print("Welcome to my pc python quiz!")
+
+playing = input("Do you want to play? ")
+
+if not (playing == "yes" or playing == "y" or playing == "Yes" or playing == "Y"):
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
+answer = input("")
+if answer == "":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
+answer = input("")
+if answer == "":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
+answer = input("")
+if answer == "":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+
+
+# def question_frame(quest):
+#     if answer == quest:
+#         print("Correct!")
+#     else:
+#         print("Incorrect!")
